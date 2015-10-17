65eb8d97fa1b8e54be67f136ec820e3ee008d6e6
Paul Pechin <ppechin@usnews.com>
2015-10-17 12:42:38 -0400

    Tweaking test case.

--- test/spec/nunjucks-loader.js
+++ test/spec/nunjucks-loader.js
@@ -11,10 +11,11 @@ var templateTwo = require('villain/default.nunj');
 describe('nunjucks-loader', function () {
     describe('when requiring templates with the same base name', function() {
         it('should have unique entries in nunjucksPrecompiled', function() {
-            // The this.context used in the loader comes from the webpack config's
-            // context, right?
-            var keyOne = path.relative(config.context, 'hero/default.nunj');
-            var keyTwo = path.relative(config.context, 'villain/default.nunj');
+            var resourcePathOne = path.resolve(__dirname + 'fixtures/templates/hero/default.nunj');
+            var resourcePathTwo = path.resolve(__dirname + 'fixtures/templates/villain/default.nunj');
+
+            var keyOne = path.relative(config.context, resourcePathOne);
+            var keyTwo = path.relative(config.context, resourcePathTwo);
 
             nunjucks.nunjucksPrecompiled.should.include.keys([keyOne, keyTwo]);
         });
